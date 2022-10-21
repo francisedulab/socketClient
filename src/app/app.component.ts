9623e2effd6d54f3757ba12805d7b60cc4ce0dd6
francisedulab <francis@edulab.in>
2022-10-21 15:17:43 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit  } from '@angular/core';
+import { io } from "socket.io-client";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'socketClient';
+  ngOnInit(): void {
+    const socket = io("http://localhost:3000" , { transports: ['websocket'] });
+    // receive a message from the server
+    socket.on("hello", (arg) => {
+    console.log(arg); // prints "world"
+    });
+    // send a message to the server
+    socket.emit("howdy", "stranger things");
+  }
+
 }
